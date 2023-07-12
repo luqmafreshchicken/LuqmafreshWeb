880d69088ee4d311bcd9048a3459b9331da85142
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-07-12 19:38:33 +0530

    Update AmazonPay.js

--- src/component/payment/paymentcomponent/amazonpay/AmazonPay.js
+++ src/component/payment/paymentcomponent/amazonpay/AmazonPay.js
@@ -6,24 +6,80 @@ const AmazonPay = () => {
 
   return (
     <div className="amazon_container">
-      {/* heading */}
-      {/* <div className="amazon_heading">
-        <p>Amazon Pay</p>
-  </div>*/}
-      {/* end heading */}
       {/* radio & amazon image */}
       <div className="amazon_select_content">
-        <div className="amazon_radio_btn"></div>
+        <div className="radio_amazon_image">
+          {/* amazon caontainer */}
+          <div className="amazon_radio_btn"></div>
+          <div className="amazon_image_container">
+            <img src="Amazon.svg" height="100px" width="200px" />
+          </div>
+          <div
+            className="amazon_linkaccount_text"
+            onClick={() => setAmazon(true)}
+          >
+            <p>Link Account</p>
+          </div>
+          {/* end amazon caontainer */}
+        </div>
+      </div>
+      {amazon == true ? (
+        <div className="amazon_inputotp_container">
+          <div className="amazon_inputotp_content">
+            <div className="input_resend">
+              <input placeholder="Enter OTP" type="" />
+              <p> Resend </p>
+            </div>
+          </div>
+        </div>
+      ) : null}
+      {amazon == true ? (
+        <div className="amazon_payment_btn_container">
+          <div className="amazon_payment_btn_content">
+            <div className="amazon_payment_btn">
+              <p>4653.33</p>
+            </div>
+          </div>
+        </div>
+      ) : null}
+      {amazon == false ? (
+        <div className="amazon_payment_btn_container">
+          <div className="amazon_payment_btn_content">
+            <div
+              className="amazon_payment_btn"
+              style={{ backgroundColor: "#a2a2a2", color: "white" }}
+            >
+              <p>653.33</p>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default AmazonPay;
+
+{
+  /*
+ <div className="amazon_radio_btn"></div>
         <div className="amazon_image_container">
-          <img src="Amazon.svg" />
+        <img src="Amazon.svg" height="100px" width="200px"/>
         </div>
         <div className="amazon_link_account" onClick={() => setAmazon(true)}>
           <p>Link Account</p>
         </div>
-      </div>
-      {/* end radio & amazon image */}
-      {/* input otp */}
-      {amazon == true ? (
+*/
+}
+
+{
+  /* end radio & amazon image */
+}
+{
+  /* input otp */
+}
+{
+  /*amazon == true ? (
         <div className="amazon_inputotp_container">
           <div className="amazon_inputotp_content">
             <input placeholder="Enter OTP" type="" />
@@ -31,8 +87,10 @@ const AmazonPay = () => {
           </div>
         </div>
       ) : null}
-      {/* end input otp */}
-      {amazon == true ? (
+      {/* end input otp */
+}
+{
+  /*amazon == true ? (
         <div className="amazon_payment_btn_container">
           <p>1246.65</p>
         </div>
@@ -43,9 +101,5 @@ const AmazonPay = () => {
         >
           <p>1246.65</p>
         </div>
-      )}
-    </div>
-  );
-};
-
-export default AmazonPay;
+      )*/
+}
