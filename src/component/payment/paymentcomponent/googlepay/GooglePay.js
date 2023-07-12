8efd8a597ba6eeebc64f529eab684229563e8e93
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-07-12 19:40:04 +0530

    Update GooglePay.js

--- src/component/payment/paymentcomponent/googlepay/GooglePay.js
+++ src/component/payment/paymentcomponent/googlepay/GooglePay.js
@@ -1,51 +1,63 @@
-import React, { useState } from "react";
 import "./googlepay.css";
+import React, { useState } from "react";
+
 
 const GooglePay = () => {
-  const [googlepay, setgooglepay] = useState(false);
+  const [googlepay, setGooglepay] = useState(false);
 
   return (
     <div className="googlepay_container">
-      {/* heading */}
-      {/* <div className="googlepay_heading">
-        <p>googlepay Pay</p>
-  </div>*/}
-      {/* end heading */}
       {/* radio & googlepay image */}
       <div className="googlepay_select_content">
-        <div className="googlepay_radio_btn"></div>
-        <div className="googlepay_image_container">
-          <img src="GooglePay.png" />
-        </div>
-        <div className="googlepay_link_account" onClick={() => setgooglepay(true)}>
-          <p>Link Account</p>
+        <div className="radio_googlepay_image">
+          {/* googlepay caontainer */}
+          <div className="googlepay_radio_btn"></div>
+          <div className="googlepay_image_container">
+            <img src="GooglePay.png" height="30px" width="150px" />
+          </div>
+          <div
+            className="googlepay_linkaccount_text"
+            onClick={() => setGooglepay(true)}
+          >
+            <p>Link Account</p>
+          </div>
+          {/* end googlepay caontainer */}
         </div>
       </div>
-      {/* end radio & googlepay image */}
-      {/* input otp */}
       {googlepay == true ? (
         <div className="googlepay_inputotp_container">
           <div className="googlepay_inputotp_content">
-            <input placeholder="Enter OTP" type="" />
-            <p> Resend </p>
+            <div className="input_resend">
+              <input placeholder="Enter OTP" type="" />
+              <p> Resend </p>
+            </div>
           </div>
         </div>
       ) : null}
-      {/* end input otp */}
       {googlepay == true ? (
         <div className="googlepay_payment_btn_container">
-          <p>1246.65</p>
+          <div className="googlepay_payment_btn_content">
+            <div className="googlepay_payment_btn">
+              <p>4653.33</p>
+            </div>
+          </div>
         </div>
-      ) : (
-        <div
-          className="googlepay_payment_btn_container"
-          style={{ backgroundColor: "#a2a2a2", color: "white" }}
-        >
-          <p>1246.65</p>
+      ) : null}
+      {googlepay == false ? (
+        <div className="googlepay_payment_btn_container">
+          <div className="googlepay_payment_btn_content">
+            <div
+              className="googlepay_payment_btn"
+              style={{ backgroundColor: "#a2a2a2", color: "white" }}
+            >
+              <p>653.33</p>
+            </div>
+          </div>
         </div>
-      )}
+      ) : null}
     </div>
   );
 };
 
 export default GooglePay;
+
