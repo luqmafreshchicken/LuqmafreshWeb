88bb6340219d36c9c3d31947ec8d7cdf0deea3ff
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-07-25 14:09:58 +0530

    Update TodayDeals.js

--- src/pages/todaydeals/TodayDeals.js
+++ src/pages/todaydeals/TodayDeals.js
@@ -7,6 +7,7 @@ import {
   getAllSubcategoryByCategoryId,
   getUserID,
   ProductBySubCategoryId,
+  productbyCategorie,
 } from "../../serverRequest/Index";
 import Header from "../../component/header/Header";
 import Loader from "../../component/loder/Loader";
@@ -14,15 +15,18 @@ import * as moment from "moment";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Card from "../../customcomponent/card/Card";
+import ProductNotFound from "../../customcomponent/productnotfound/ProductNotFound";
 
 const TodayDeals = () => {
   let location = useLocation();
   const [subcategorie, setSubcategorie] = useState([]);
   const [product, setProduct] = useState([]);
-  const [load, setLoad] = useState(false); 
+  const [load, setLoad] = useState(false);
 
   useEffect(() => {
+    setLoad(true);
     subCategorie();
+    productAll();
     window.scrollTo(0, 0);
   }, []);
 
@@ -35,7 +39,8 @@ const TodayDeals = () => {
     getAllSubcategoryByCategoryId(requestData).then((res) => {
       if (res.status == true) {
         setSubcategorie(res.data);
-        categoryProduct(res?.data[0]?._id);
+        setLoad(false);
+        // categoryProduct(res?.data[0]?._id);
       } else {
         setLoad(false);
       }
@@ -49,6 +54,25 @@ const TodayDeals = () => {
       subCategoryId: id,
     };
     ProductBySubCategoryId(requestData).then((res) => {
+      if (res.status == true) {
+        setProduct(res.data);
+        setLoad(false);
+      } else {
+        setLoad(false);
+      }
+    });
+  };
+
+  const productAll = async (id) => {
+    setLoad(true);
+    const categoryId = location.state.id.id;
+
+    const requestData = {
+      categoryId: categoryId,
+    };
+    console.log(requestData, "=============1234567============");
+
+    productbyCategorie(requestData).then((res) => {
       if (res.status == true) {
         setProduct(res.data);
         setLoad(false);
@@ -94,41 +118,51 @@ const TodayDeals = () => {
     <>
       <Header />
       <div>
-        <div className="subcategorie_contaner">
-          <div className="subcategorie_content">
-            {subcategorie.map((category) => (
-              <SubCategorieList
-                // img={category.subcategoryImage}
-                name={category.subcategoryName}
-                onclick={() => categoryProduct(category._id)}
-              />
-            ))}
-          </div>
-        </div>
-        <div className="main_today_card">
-          <div className="today_card">
-            {product.map((item) => (
-              <Card
-                offer={item.discount}
-                productName={item.name}
-                weight={item.quantity + "  " + item.unit}
-                total={item.price}
-                cutotal={item.originalPrice}
-                offer1={item.discount}
-                today={moment(item.discountExpiryDate).format("dddd")}
-                date={item.deliveryTime}
-                totalpayment={item.price}
-                img={item.image}
-                rating={item.rating}
-                id={{ id: item._id }}
-                to="/carddetail"
-                onclick={() => handleCart(item._id)}
-              />
-             
-            ))}
+        {subcategorie.length >= 1 ? (
+          <div className="subcategorie_contaner">
+            <div className="subcategorie_content">
+              {subcategorie.map((category) => (
+                <SubCategorieList
+                  // img={category.subcategoryImage}
+                  name={category.subcategoryName}
+                  onclick={() => categoryProduct(category._id)}
+                />
+              ))}
+            </div>
             <Loader loading={load} />
           </div>
-        </div>
+        ) : null}
+
+        {product.length >= 1 ? (
+          <div className="main_today_card">
+            <div className="today_card">
+              {product.map((item) => (
+                <Card
+                  offer={item.discount}
+                  productName={item.name}
+                  weight={item.quantity + "  " + item.unit}
+                  total={item.price}
+                  cutotal={item.originalPrice}
+                  offer1={item.discount}
+                  today={moment(item.discountExpiryDate).format("dddd")}
+                  date={item.deliveryTime}
+                  totalpayment={item.price}
+                  img={item.image}
+                  rating={item.rating}
+                  id={{ id: item._id }}
+                  to="/carddetail"
+                  onclick={() => handleCart(item._id)}
+                />
+              ))}
+              <Loader loading={load} />
+            </div>
+          </div>
+        ) : (
+          <>
+            <ProductNotFound />
+          </>
+        )}
+
         <ToastContainer
           position="top-right"
           autoClose={5000}
