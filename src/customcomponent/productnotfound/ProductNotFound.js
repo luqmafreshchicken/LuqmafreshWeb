cca004d918b5ec0b0e850ec36cb73467f9948982
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-07-25 15:10:37 +0530

    Add files via upload

---
+++ src/customcomponent/productnotfound/ProductNotFound.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './productnotfound.css'
+
+const ProductNotFound = () => {
+  return (
+    <div className='productnotfound_container'>
+    <div  className='productnotfound_content'>
+        <h4>Product Upload Soon </h4>
+        </div>
+    </div>
+  )
+}
+
+export default ProductNotFound
