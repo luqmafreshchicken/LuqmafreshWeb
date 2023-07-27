349bb0ae8731cd3bcd2be3cb782079f0ac834581
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-07-27 14:45:31 +0530

    Update CardDetail.js

--- src/pages/carddetail/CardDetail.js
+++ src/pages/carddetail/CardDetail.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Card from "../../customcomponent/card/Card";
 
 import CardFullDetail from "../../customcomponent/cardfulldetail/CardFullDetail";
@@ -7,12 +7,16 @@ import "./carddetail.css";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import { Pagination } from "swiper";
 import "swiper/css/navigation";
 import { Add_to_cart, getUserID, newArrival } from "../../serverRequest/Index";
 import * as moment from "moment";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import Text from "../../component/text/Text";
+import { Navigation, Parallax } from "swiper";
+
+
 
 const CardDetail = () => {
   const [data, setData] = useState([]);
@@ -56,43 +60,94 @@ const CardDetail = () => {
       });
     }
   };
+  const swiperNavPrevRef = useRef(null);
+  const swiperNavNextRef = useRef(null);
   return (
     <div>
       <CardFullDetail />
       <ImagesCard />
-      <div className="carddetail_slider_heading_container">
-        <div className="carddetail_slider_heading_content">
-          <h4>You May Also Like</h4>
+      <div>
+        <div className="next_prev_btn_container">
+          <div className="next_prev_btn">
+            <div className="head_box">
+              <Text
+                heading1="You May Also Like"
+                text1="Most popular products near you!"
+              />
+            </div>
+            <div className="next_prev_btn_content">
+              <div className="swiperNavPrev" ref={swiperNavPrevRef}>
+                <FaArrowLeft className="FaArrowLeft" />
+              </div>
+              <div className="swiperNavNext" ref={swiperNavNextRef}>
+                <FaArrowRight className="FaArrowRight" />
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="carddetail_slider_container">
-        <div className="carddetail_slider_content">
-          {data.length >= 1 ? (
-            <>
-              {data.map((detail, index) => (
-                <Card
-                  offer={detail.discount}
-                  productName={detail.name}
-                  weight={detail.quantity}
-                  unit={detail.unit}
-                  total={detail.price}
-                  cutotal={detail.originalPrice}
-                  offer1={detail.discount}
-                  today={moment(detail.discountExpiryDate).format("dddd")}
-                  date={detail.deliveryTime}
-                  totalpayment={detail.price}
-                  to="/carddetail"
-                  onclick={() => AddToCart(detail._id)}
-                  id={{ id: detail._id }}
-                  rating={detail.rating}
-                  img={detail.image}
-                />
-              ))}
-            </>
-          ) : null}
-          <div className="slider_next_btn1">
-          <img src="rtarrow.png" height="20px" width="20px" />
-        </div>
+        <div className="carouselitem">
+          <div className="cardswrapper">
+            <Swiper
+              slidesPerView={1}
+              spaceBetween={8}
+              pagination={{
+                clickable: true,
+              }}
+              parallax={true}
+              navigation={{
+                prevEl: "swiperNavPrevRef.current",
+                nextEl: "swiperNavNextRef.current",
+              }}
+              onInit={(swiper) => {
+                swiper.params.navigation.prevEl = swiperNavPrevRef.current;
+                swiper.params.navigation.nextEl = swiperNavNextRef.current;
+                swiper.navigation.init();
+                swiper.navigation.update();
+              }}
+              breakpoints={{
+                640: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 4,
+                  spaceBetween: 40,
+                },
+              }}
+            
+              modules={[Navigation, Parallax]}
+            >
+              {data.length >= 1 ? (
+                <>
+                  {data.map((detail, index) => (
+                    <SwiperSlide>
+                      <Card
+                        offer={detail.discount}
+                        productName={detail.name}
+                        weight={detail.quantity}
+                        unit={detail.unit}
+                        total={detail.price}
+                        cutotal={detail.originalPrice}
+                        offer1={detail.discount}
+                        today={moment(detail.discountExpiryDate).format("dddd")}
+                        date={detail.deliveryTime}
+                        totalpayment={detail.price}
+                        to="/carddetail"
+                        onclick={() => AddToCart(detail._id)}
+                        id={{ id: detail._id }}
+                        rating={detail.rating}
+                        img={detail.image}
+                        // onclick1={() => fullView(detail._id)}
+                        // onclick2={() => setWhistlistOpen(true)}
+                      />
+                    </SwiperSlide>
+                  ))}
+                </>
+              ) : null}
+            </Swiper>
+          </div>
+         
+        
         </div>
       </div>
     </div>
