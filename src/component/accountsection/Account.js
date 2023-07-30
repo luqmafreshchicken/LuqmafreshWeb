cf1a27d66805dce5265c5a2f5b87b5c979c8ef74
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-07-30 19:31:35 +0530

    Update Account.js

--- src/component/accountsection/Account.js
+++ src/component/accountsection/Account.js
@@ -5,24 +5,45 @@ import ViewProfile from "../viewprofile/ViewProfile";
 import { useState } from "react";
 import Orderhistory from "../orderhistory/Orderhistory";
 import Header from "../header/Header";
-import { viewProfile, getUserID } from "../../serverRequest/Index";
+import {
+  viewProfile,
+  getUserID,
+  CountryDetail,
+  GetCountry,
+  Show_Cart,
+} from "../../serverRequest/Index";
 import Notification from "../notification/Notification";
 import WhistListDetail from "../whistlistdetail/WhistListDetail";
+import { useNavigate } from "react-router-dom";
+
 
 const Account = () => {
+  let navigate = useNavigate();
+
   const [profile, setProfile] = useState(false);
-  const [open, setOpen] = useState(0);
+  const [open2, setOpen2] = useState(0);
   const [viewUser, setViewUser] = useState([]);
+  const [country, setCountry] = useState("");
+  const [countrycurrency, setCountryCurrency] = useState("");
+  const [countrytitle, setCountryTitle] = useState("");
+  const [flag, setFlag] = useState("");
+  const [cartProduct, setCartProduct] = useState([]);
+  const [cartPrice, setCartPrice] = useState([]);
+  const [cartOpen, setCartOpen] = useState(false);
+  const [btn, setBtn] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [loginStatus, setLoginStatus] = useState(false);
 
   const handleOpen = () => {
-    if (open === 0) {
+    if (open2 === 0) {
       return <Orderhistory />;
-    }if (open === 4) {
-      return <Notification/>
-    } 
-    if (open === 6) {
-      return <WhistListDetail/>
-    } 
+    }
+    if (open2 === 1) {
+      return <Notification />;
+    }
+    if (open2 === 2) {
+      return <WhistListDetail />;
+    }
   };
   useEffect(() => {
     userDetail();
@@ -31,21 +52,111 @@ const Account = () => {
   const userDetail = async () => {
     const UserId = await getUserID();
     viewProfile(UserId).then((res) => {
+      console.log(res.data);
       if (res.status == true) {
         setViewUser(res.data);
       } else {
       }
     });
   };
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          if (position?.coords?.latitude) {
+            GetCountry(
+              position?.coords?.latitude,
+              position?.coords?.longitude
+            ).then((res) => {
+              if (res?.address?.country) {
+                CountryDetail(res?.address?.country).then((res) => {
+                  setCountry(res[0]?.name);
+                  setCountryCurrency(res[0]?.currencies[0]?.symbol);
+                  setCountryTitle(res[0]?.currencies[0]?.code);
+                  setFlag(res[0]?.flags?.png);
+                });
+              }
+            });
+          }
+        },
+        (error) => {
+          console.error("Error retrieving location:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by your browser.");
+    }
+    localContent();
+    showcart();
+  }, []);
+  const localContent = () => {
+    const items = JSON.parse(localStorage.getItem("userDetail"));
+    const items1 = JSON.parse(localStorage.getItem("modalCount"));
+    if (items) {
+      setLoginStatus(true);
+    } else {
+      setLoginStatus(false);
+      if (items1) {
+      } else {
+        setLoginStatus(false);
+      }
+    }
+  };
+  const showcart = async () => {
+    const userId = await getUserID();
+    const data = {
+      userId: userId,
+    };
+    const res = await Show_Cart(data);
+    if (res.status == true) {
+      setCartProduct(res.data.cart);
+      setCartPrice(res.data.totalAmount);
+    } else {
+      setCartProduct([]);
+      setCartPrice("");
+    }
+  };
+
+  const carthandleOpen = () => setCartOpen(true);
+  const carthandleClose = () => setCartOpen(false);
+
+  const handleEdit = async (id) => {
+    console.log(id);
+  };
+  const handleclear = async (index) => {
+    if (index == 4) {
+      await localStorage.clear();
+      navigate("/");
+      window.location.reload();
+    }
+  };
   const viewhandleOpen = () => setProfile(true);
   const viewhandleClose = () => setProfile(false);
   return (
     <>
       <div className="account_header">
-        <Header />
+        <Header
+          code={countrytitle}
+          currency={countrycurrency}
+          flag={flag}
+          cartPrice={cartPrice}
+          cartProductlength={cartProduct}
+          curr={countrycurrency}
+          cartopen={cartOpen}
+          carthandleClose={carthandleClose}
+          carthandleOpen={carthandleOpen}
+          loginStatus={loginStatus}
+          handleOpen={() => setOpen(true)}
+          handleClose={() => setOpen(false)}
+          open={open}
+          showbtn={btn}
+          totalAmount={cartPrice}
+          handleclear={(index) => handleclear(index)}
+        />
       </div>
-      <div className="account_section" >
-        <div className="account_content" >
+      <div className="account_section">
+        <div className="account_content">
           <div className="account_text">
             <h3>{viewUser.name}</h3>
             <div className="account_profile">
@@ -78,9 +189,8 @@ const Account = () => {
                     }}
                   >
                     <div
-                     
                       className="hover_bottom"
-                      onClick={() => setOpen(index)}
+                      onClick={() => setOpen2(index)}
                     >
                       {list.routeName}
                     </div>
@@ -88,7 +198,7 @@ const Account = () => {
                 ))}
               </ul>
             </div>
-            <div className="show_data">{handleOpen(open)}</div>
+            <div className="show_data">{handleOpen(open2)}</div>
           </div>
           {/* end show data */}
         </div>
