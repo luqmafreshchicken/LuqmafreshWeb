6505487f90c2acb4a46286c608859796a3953a8d
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-07-31 19:00:09 +0530

    Update ModalCart.js

--- src/pages/modalcart/ModalCart.js
+++ src/pages/modalcart/ModalCart.js
@@ -39,83 +39,81 @@ const ModalCart = ({
         aria-describedby="modal-modal-description"
         onClose={onclose}
       >
-        <Box className="style2">
-          <button onClick={carthandleClose} className="modalcart_btn">
-            <img src="cross.png" className="close_modal" />
-          </button>
-          {loginStatus == true ? (
-            <>
-              {cartProduct?.length >= 1 ? (
-                <div className="modalcart_scroll_bar_container">
-                  <div className="order_summary">
-                    <p>Order Summary</p>
-                  </div>
+        <Box>
+          <div className="style2">
+            {loginStatus == true ? (
+              <>
+                {cartProduct?.length >= 1 ? (
+                  <div className="modalcart_scroll_bar_container">
+                    <div className="order_summary">
+                      <p>Your Order List</p>
+                    </div>
 
-                  {cartProduct.map((option, index) => (
-                    <div className="product_card">
-                      <div className="product_detail">
-                        <div className="subproduct_detail">
-                          <div className="number">
-                            <p>{index + 1}</p>
+                    {cartProduct.map((option, index) => (
+                      <div className="product_card">
+                        <div className="product_detail">
+                          <div className="subproduct_detail">
+                            <div className="number">
+                              <p>{index + 1}</p>
+                            </div>
+
+                            <div className="product_name">
+                              <p>{option.productId.name}</p>
+                            </div>
                           </div>
 
-                          <div className="product_name">
-                            <p>{option.productId.name}</p>
+                          <div
+                            onClick={() => removeProduct(option._id)}
+                            className="product_cross"
+                          >
+                            <img src="cross.png" height="22px" width="22px" />
                           </div>
                         </div>
-
-                        <div
-                          onClick={() => removeProduct(option._id)}
-                          className="product_cross"
-                        >
-                          <img src="cross.png" height="22px" width="22px" />
+                        <div className="peices_content">
+                          <div className="peices_price">
+                            <div className="border_peices">
+                              <p>{option.productId.quantity}gms</p>
+                            </div>
+                            <div className="color_count">
+                              <p>
+                                {" "}
+                                {modalcurrency}
+                                {option.productId.price}
+                              </p>
+                            </div>
+                            <div className="cut_count">
+                              <p>
+                                {" "}
+                                {modalcurrency}
+                                {option.productId.originalPrice}
+                              </p>
+                            </div>
+                          </div>
                         </div>
                       </div>
-                      <div className="peices_content">
-                        <div className="peices_price">
-                          <div className="border_peices">
-                            <p>{option.productId.quantity}gms</p>
-                          </div>
-                          <div className="color_count">
-                            <p>
-                              {" "}
-                              {modalcurrency}
-                              {option.productId.price}
-                            </p>
-                          </div>
-                          <div className="cut_count">
-                            <p>
-                              {" "}
-                              {modalcurrency}
-                              {option.productId.originalPrice}
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
+                    ))}
 
-                  <div className="biil_payment">
-                    <div className="bill_name">
-                      <p>Bill Details</p>
-                    </div>
-                    <div className="subbill_Detail">
-                      <p style={{ paddingLeft: "15px", paddingTop: "15px" }}>
-                        Amount {modalcurrency}
-                      </p>
-                      <p style={{ paddingRight: "15px", paddingTop: "15px" }}>
-                        {totalAmount}
-                      </p>
-                    </div>
-                    <div className="subbill_Detail">
-                      <p style={{ paddingLeft: "15px", paddingTop: "15px" }}>
-                        Vat {modalcurrency}
-                      </p>
-                      <p style={{ paddingRight: "15px", paddingTop: "15px" }}>
-                        {(totalAmount * 5) / 100}
-                      </p>
-                    </div>
-                    {/* <div className="delivery_charge">
+                    <div className="biil_payment">
+                      <div className="bill_name">
+                        <p>Bill Details</p>
+                      </div>
+                      <div className="subbill_Detail">
+                        <p style={{ paddingLeft: "15px", paddingTop: "15px" }}>
+                          Amount {modalcurrency} :
+                        </p>
+                        <p style={{ paddingRight: "15px", paddingTop: "15px" }}>
+                          {totalAmount}
+                        </p>
+                      </div>
+                      <div className="subbill_Detail">
+                        <p style={{ paddingLeft: "15px", paddingTop: "15px" }}>
+                          Vat {modalcurrency} :
+                        </p>
+                        <p style={{ paddingRight: "15px", paddingTop: "15px" }}>
+                          {(totalAmount * 5) / 100}
+                        </p>
+                      </div>
+                      {/* <div className="delivery_charge">
                       <p style={{ paddingLeft: "15px", paddingTop: "10px" }}>
                         Delivery Charge
                       </p>
@@ -124,76 +122,83 @@ const ModalCart = ({
                         {totalAmount > 200 ? 0 : 50}
                       </p>
                     </div> */}
-                    <div className="cong_charge_section">
-                      <p style={{ paddingLeft: "15px" }}>
-                        Congratulations, Your delivery charge is waived off!!!
-                      </p>
-                    </div>
-                    <div className="border_line"></div>
-                    <div className="total_payments">
-                      <p style={{ paddingTop: "10px" }}>Total Amount {modalcurrency} </p>
-                      <p style={{ paddingTop: "10px", color: "#FF0040" }}>
-                        {/* {modalcurrency}
+                      <div className="cong_charge_section">
+                        <p style={{ paddingLeft: "15px" }}>
+                          Congratulations, Your delivery charge is waived off!!!
+                        </p>
+                      </div>
+                      <div className="border_line"></div>
+                      <div className="total_payments">
+                        <p style={{ paddingTop: "10px" }}>
+                          Total Amount {modalcurrency} :
+                        </p>
+                        <p style={{ paddingTop: "10px", color: "#FF0040" }}>
+                          {/* {modalcurrency}
                         {totalAmount > 200
                           ? totalAmount + (totalAmount * 5) / 100
                           : totalAmount + 50 + (totalAmount * 5) / 100} */}
-                        {totalAmount + (totalAmount * 5) / 100}
-                      </p>
+                          {totalAmount + (totalAmount * 5) / 100}
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                  {cartProduct?.length >= 1 ? (
-                    <div className="proceed_section">
-                      <div className="proceed_payment">
-                        <p>
-                          {/* {" "}
+                    {cartProduct?.length >= 1 ? (
+                      <div className="proceed_section">
+                        <div className="proceed_payment">
+                          <p>
+                            {/* {" "}
                           Total : {modalcurrency}
                           {totalAmount > 200
                             ? totalAmount + (totalAmount * 5) / 100
                             : totalAmount + 50 + (totalAmount * 5) / 100} */}
-                            {modalcurrency} {totalAmount + (totalAmount * 5) / 100}
-                        </p>
+                            {modalcurrency}{" "}
+                            {totalAmount + (totalAmount * 5) / 100}
+                          </p>
+                        </div>
+                        <div className="proceed_btn" onClick={onclick}>
+                          <NavLink to="/addnewaddress" className="nav_list">
+                            <p> Checkout</p>
+                          </NavLink>
+                        </div>
                       </div>
-                      <div className="proceed_btn" onClick={onclick}>
-                        <NavLink to="/addnewaddress" className="nav_list">
-                          <p>Proceed to Checkout</p>
-                        </NavLink>
-                      </div>
+                    ) : null}
+                  </div>
+                ) : (
+                  <>
+                    <div className="mobile_cart_nologin">
+                      <img src="emty cart.png" />
                     </div>
-                  ) : null}
-                </div>
-              ) : (
-                <>
-                  <div className="mobile_cart_nologin">
-                    <img src="emty cart.png" />
-                  </div>
-                  <div className="cart_empty">
-                    <h5>Your cart is empty</h5>
-                    <p>
-                      It appears that your cart is currently empty.
-                      <br /> Feel free to explore our top categories and add
-                      items to your cart.
-                    </p>
-                  </div>
-                </>
-              )}
-            </>
-          ) : null}
+                    <div className="cart_empty">
+                      <h5>Your cart is empty</h5>
+                      <p>
+                        It appears that your cart is currently empty.
+                        <br /> Feel free to explore our top categories and add
+                        items to your cart.
+                      </p>
+                    </div>
+                  </>
+                )}
+              </>
+            ) : null}
 
-          {loginStatus == false ? (
-            <>
-              <div className="mobile_cart_nologin">
-                <img src="lock.png" />
-              </div>
-              <div className="cart_empty">
-                <h5>Please Login First</h5>
-                <p>
-                  It seems that you haven't logged in yet. Please feel welcome
-                  to log in and explore our premium categories, as well as add
-                  items to your cart.
-                </p>
-              </div>
-            </>
-          ) : null}
+            {loginStatus == false ? (
+              <>
+                <div className="mobile_cart_nologin">
+                  <img src="lock.png" />
+                </div>
+                <div className="cart_empty">
+                  <h5>Please Login First</h5>
+                  <p>
+                    It seems that you haven't logged in yet. Please feel welcome
+                    to log in and explore our premium categories, as well as add
+                    items to your cart.
+                  </p>
+                </div>
+              </>
+            ) : null}
+          </div>
+          <div className="crossbtn" onClick={carthandleClose}>
+            <img src="crossbtn.png" />
+          </div>
         </Box>
       </Modal>
     </div>
