a2a96ec319aa7c92210b8206aa4dc43c89b7bdbb
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-07-31 14:03:36 +0530

    Add files via upload

---
+++ src/pages/youmaylikedetail/YouMayLikeDetail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import YouMayLikeFullView from '../../customcomponent/cardfulldetail/YouMayLikeFullView'
+
+const YouMayLikeDetail = () => {
+  return (
+    <div>
+      <YouMayLikeFullView/>
+    </div>
+  )
+}
+
+export default YouMayLikeDetail
