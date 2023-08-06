cff88f2d31544429eaea2c2c25eadc015bce517e
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-08-07 01:12:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,24 +6,16 @@ import HeaderFooter from "./component/headerfooter/HeaderFooter";
 import LowerFooter from "./component/lowerfooter/LowerFooter";
 import NewsFooter from "./component/newsfooter/NewsFooter";
 import SupportSection from "./component/supportsection/SupportSection";
-import TopHeader from "./component/topheader/TopHeader";
 import GoToTop from "./customcomponent/gototop/GoToTop";
 import MobileBottomtab from "./mobilecomponent/mobilebottomtab/MobileBottomtab";
 import Routes from "./pages/routes/Routes";
 import "./App.css";
-import { useNavigate } from "react-router-dom";
 
 function App() {
-  let navigate = useNavigate();
 
-  const handleClear = async () => {
-    await localStorage.clear();
-    navigate("/");
-    window.location.reload();
-  };
+
   return (
     <div>
-      <TopHeader handleClear={() => handleClear()} />
       <Routes />
       <div className="mobile_news_footer">
         <NewsFooter />
