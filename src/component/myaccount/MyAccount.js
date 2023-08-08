3d12810926332e57a9e2015b48647e54a4556222
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-08-08 16:58:46 +0530

    Update MyAccount.js

--- src/component/myaccount/MyAccount.js
+++ src/component/myaccount/MyAccount.js
@@ -1,13 +1,62 @@
 import React from "react";
 import "./myaccount.css";
 import { useEffect, useState } from "react";
-import { currentLocation, getUserID, viewProfile } from "../../serverRequest/Index";
+import {
+  CountryDetail,
+  GetCountry,
+  currentLocation,
+  getUserID,
+  viewProfile,
+} from "../../serverRequest/Index";
+import Loader from "../loder/Loader";
 const MyAccount = () => {
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
   const [city, setCity] = useState("");
   const [viewUser, setViewUser] = useState([]);
+  const [country, setCountry] = useState("");
+  const [countryCode, setCountryCode] = useState("");
+  const [countrycurrency, setCountryCurrency] = useState("");
+  const [countrytitle, setCountryTitle] = useState("");
+  const [flag, setFlag] = useState("");
+  const [load, setLoad] = useState("");
 
+  useEffect(() => {
+    // localContent();
+    window.scrollTo(0, 0);
+    setLoad(true);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          if (position?.coords?.latitude) {
+            GetCountry(
+              position?.coords?.latitude,
+              position?.coords?.longitude
+            ).then((res) => {
+              if (res?.address?.country) {
+                CountryDetail(res?.address?.country).then((res) => {
+                  setCountry(res[0]?.name);
+                  // setCountryCode(res[0]?.countryCode[0].code)
+                  setCountryCurrency(res[0]?.currencies[0]?.symbol);
+                  setCountryTitle(res[0]?.currencies[0]?.code);
+                  setFlag(res[0]?.flags?.png);
+                  setLoad(false);
+                });
+              }
+            });
+          }
+        },
+        (error) => {
+          console.error("Error retrieving location:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by your browser.");
+    }
+    // localContent();
+    // showcart();
+    // arrivalProductList();
+  }, []);
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -31,7 +80,7 @@ const MyAccount = () => {
     } else {
       console.error("Geolocation is not supported by your browser.");
     }
-    userDetail()
+    userDetail();
   });
 
   const userDetail = async () => {
@@ -62,12 +111,16 @@ const MyAccount = () => {
 
       <div className="myaccount_username_container">
         <p>Mobile :</p>
-        <p>+91 {viewUser?.mobile?.number}</p>
+        <p>
+         
+          <img src={flag} /> +971 {viewUser?.mobile?.number}
+        </p>
       </div>
       <div className="myaccount_username_container">
         <p>Address :</p>
         <p>{city}</p>
       </div>
+      <Loader loading={load} />
     </>
   );
 };
