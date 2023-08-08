8e99e51cdd72e76ae637fb3d42d73fa5c2d695bb
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-08-08 17:01:05 +0530

    Update ViewProfile.js

--- src/component/viewprofile/ViewProfile.js
+++ src/component/viewprofile/ViewProfile.js
@@ -3,16 +3,53 @@ import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import "./viewprofile.css";
 import Editprofile from "../editprofile/Editprofile";
-import { viewProfile, getUserID } from "../../serverRequest/Index";
+import { viewProfile, getUserID, CountryDetail, GetCountry } from "../../serverRequest/Index";
 
 const ViewProfile = ({ profile, viewhandleClose }) => {
   const [edit, setEdit] = useState(false);
   const [viewUser, setViewUser] = useState([]);
-
+  // const [country, setCountry] = useState("");
+  // const [countrycurrency, setCountryCurrency] = useState("");
+  // const [countrytitle, setCountryTitle] = useState("");
+  const [flag, setFlag] = useState("");
+  
   useEffect(() => {
+    // localContent();
+    // window.scrollTo(0, 0);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          if (position?.coords?.latitude) {
+            GetCountry(
+              position?.coords?.latitude,
+              position?.coords?.longitude
+            ).then((res) => {
+              if (res?.address?.country) {
+                CountryDetail(res?.address?.country).then((res) => {
+                  // setCountry(res[0]?.name);
+                  // setCountryCode(res[0]?.countryCode[0].code)
+                  // setCountryCurrency(res[0]?.currencies[0]?.symbol);
+                  // setCountryTitle(res[0]?.currencies[0]?.code);
+                  setFlag(res[0]?.flags?.png);
+                });
+              }
+            });
+          }
+        },
+        (error) => {
+          console.error("Error retrieving location:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by your browser.");
+    }
+    // localContent();
+    // showcart();
+    // arrivalProductList();
     userDetail();
   }, []);
 
+ 
   const userDetail = async () => {
     const UserId = await getUserID();
     viewProfile(UserId).then((res) => {
@@ -72,7 +109,7 @@ const ViewProfile = ({ profile, viewhandleClose }) => {
             <div className="first_name">
               <span>
                 Mobile No <br />
-                <p>+91 {viewUser?.mobile?.number}</p>
+                <p> <img src={flag}/> +971 {viewUser?.mobile?.number}</p>
               </span>
             </div>
             {/* email_id */}
