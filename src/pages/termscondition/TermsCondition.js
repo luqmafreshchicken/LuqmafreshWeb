ff0486edc41578847b4db0d3def5f69573dbadbd
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-08-08 14:31:06 +0530

    Update TermsCondition.js

--- src/pages/termscondition/TermsCondition.js
+++ src/pages/termscondition/TermsCondition.js
@@ -1,14 +1,427 @@
 import * as React from "react";
 // import "./privacypolicy.css";
 import Header from "../../component/header/Header";
-import { useEffect } from "react";
+import TopHeader from "../../component/topheader/TopHeader";
+import { useEffect, useState } from "react";
+import {
+  CountryDetail,
+  GetCountry,
+  Show_Cart,
+  getUserID,
+  loginRegister,
+  otpVerify,
+  removeFromCart,
+  resendOTP,
+} from "../../serverRequest/Index";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
 
 export default function TermsConditions() {
-  useEffect(() => window.scrollTo(0, 0));
+  let navigate = useNavigate();
+
+  const [loginStatus, setLoginStatus] = useState(false);
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [btn, setBtn] = useState(false);
+  const [otp, setOtp] = useState("");
+  const [load, setLoad] = useState(false);
+  const [country, setCountry] = useState("");
+  const [showInput, setShowInput] = useState(false);
+  const [hideOTP, setHideOTP] = useState(false);
+  const [countrycurrency, setCountryCurrency] = useState("");
+  const [countrytitle, setCountryTitle] = useState("");
+  const [flag, setFlag] = useState("");
+  const [cartProduct, setCartProduct] = useState([]);
+  const [cartPrice, setCartPrice] = useState([]);
+  const [cartOpen, setCartOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [store, setStore] = useState(false);
+ 
+
+  useEffect(() => {
+    localContent();
+    window.scrollTo(0, 0);
+    setLoad(true);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          if (position?.coords?.latitude) {
+            GetCountry(
+              position?.coords?.latitude,
+              position?.coords?.longitude
+            ).then((res) => {
+              if (res?.address?.country) {
+                CountryDetail(res?.address?.country).then((res) => {
+                  setCountry(res[0]?.name);
+                  setCountryCurrency(res[0]?.currencies[0]?.symbol);
+                  setCountryTitle(res[0]?.currencies[0]?.code);
+                  setFlag(res[0]?.flags?.png);
+                });
+              }
+            });
+          }
+        },
+        (error) => {
+          console.error("Error retrieving location:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by your browser.");
+    }
+    // localContent();
+    showcart();
+    // arrivalProductList();
+  }, []);
+
+  const localContent = () => {
+    const items = JSON.parse(localStorage.getItem("userDetail"));
+    const items1 = JSON.parse(localStorage.getItem("modalCount"));
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    const cartPrice = JSON.parse(localStorage.getItem("cartPrice"));
+
+    if (items) {
+      // setWhistlistOpen(false);
+      setLoginStatus(true);
+    } else {
+      setCartProduct(cart ? cart : []);
+      cart?.map((item) => {
+        setCartPrice((prev) => prev + item?.productId?.price * item?.quantity);
+      });
+      setCartPrice(cart?.length > 0 ? cartPrice?.price : 0);
+      setCartPrice(cartPrice?.price);
+      localStorage.setItem(
+        "cartPrice",
+        JSON.stringify({ price: cart?.length > 0 ? cartPrice?.price : 0 })
+      );
+      setLoginStatus(false);
+      if (items1) {
+        // setWhistlistOpen(false);
+      } else {
+        // setWhistlistOpen(true);
+        setLoginStatus(false);
+      }
+    }
+  };
+
+  // Remove local cart data throught id
+  const removeLocalCart = (id) => {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    const cartPrice = JSON.parse(localStorage.getItem("cartPrice"));
+    const cartData = cart?.filter((item) => item?.productId?._id !== id);
+    const product = cart?.find((item) => item?.productId?._id === id);
+    const removeProduct = cart?.filter((item) => item?.productId?._id !== id);
+    cart?.length >= 1 &&
+      localStorage.setItem(
+        "cartPrice",
+        JSON.stringify({ price: cartPrice?.price - product?.productId?.price })
+      );
+    cart?.length < 1 &&
+      localStorage.setItem("cartPrice", JSON.stringify({ price: 0 }));
+    localStorage.setItem("cart", JSON.stringify(cartData));
+    setCartProduct(removeProduct);
+    setCartPrice(
+      cartPrice?.price -
+        product?.productId?.price * product?.productId?.quantity
+    );
+    toast.success("Product remove from cart", {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+    });
+    if (cart?.length === 1) {
+      localStorage.removeItem("cart");
+      localStorage.removeItem("cartPrice");
+      setCartProduct([]);
+      setCartPrice(0);
+    }
+    localContent();
+  };
+
+  const handleLogin = () => {
+    // email validation
+    if (mobileNumber === "") {
+      toast.error("Please enter email", {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        draggable: true,
+      });
+      return false;
+    } else if (!mobileNumber.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
+      toast.error("Please enter valid email address", {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        draggable: true,
+      });
+      return false;
+    }
+    setLoad(true);
+    let newEmail = mobileNumber;
+    const requestData = { email: mobileNumber };
+    loginRegister(requestData).then((res) => {
+      if (res.status === true) {
+        toast.success(res?.message, {
+          position: "top-right",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        setShowInput(!showInput);
+        setHideOTP(true);
+        setBtn(true);
+        setStore(newEmail);
+        setLoad(false);
+      } else {
+        toast.error(res?.message, {
+          position: "top-right",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        setLoad(false);
+      }
+    });
+  };
+  const showcart = async () => {
+    setLoad(true);
+
+    const userId = await getUserID();
+    const data = {
+      userId: userId,
+    };
+    const res = await Show_Cart(data);
+    if (res.status == true) {
+      setCartProduct(res?.data?.cart);
+      setCartPrice(res?.data?.totalAmount);
+      setLoad(false);
+    } else {
+      setLoad(false);
+    }
+  };
+
+  const handleMobileNumber = (e) => {
+    setMobileNumber(e.target.value);
+    if (e?.target?.value?.length <= 40) {
+      setBtn(false);
+    } else {
+      setBtn(true);
+    }
+  };
+  const sethandleOtp = (e) => {
+    setOtp(e.target.value);
+  };
+
+  const handleOTP = () => {
+    // otp validation
+    if (otp === "") {
+      toast.error("Please enter otp", {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        draggable: true,
+      });
+      return false;
+    } else if (!otp.match(/^[0-9]{4}$/)) {
+      toast.error("Please enter 4 digit otp number", {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        draggable: true,
+      });
+      return false;
+    }
+    setLoad(true);
+    const requestData = { email: mobileNumber, otp: otp };
+    otpVerify(requestData).then((res) => {
+      if (res?.status == true) {
+        toast.success(res?.message, {
+          position: "top-right",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        localStorage.setItem("userDetail", JSON.stringify(res.data));
+        // updatelocalcartindb();
+        // localContent();
+        // localContent1();
+        // setWhistlistOpen(false);
+        setOpen(false);
+        setLoad(false);
+        showcart();
+        // window.location.reload();
+      } else {
+        setLoad(false);
+        toast.error("Invalid OTP", {
+          position: "top-right",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      }
+    });
+  };
+  const carthandleOpen = () => setCartOpen(true);
+  const carthandleClose = () => setCartOpen(false);
+
+  const removeCartProduct = async (id) => {
+    setLoad(true);
+    const userId = await getUserID();
+    const data = {
+      userId: userId,
+      productId: id,
+    };
+    removeFromCart(data).then((res) => {
+      if (res?.status == true) {
+        toast.success(res?.message, {
+          position: "top-right",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        showcart();
+        setLoad(false);
+        // arrivalProductList();
+      } else {
+        toast.error(res?.message, {
+          position: "top-right",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      }
+    });
+  };
+  const handleclear = async (index) => {
+    if (index == 4) {
+      await localStorage.clear();
+      navigate("/");
+      window.location.reload();
+    }
+  };
+
+  const handleResendOTP = () => {
+    // email validation
+    if (mobileNumber === "") {
+      toast.error("Please enter email", {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        draggable: true,
+      });
+      return false;
+    } else if (!mobileNumber.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
+      toast.error("Please enter valid email address", {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        draggable: true,
+      });
+      return false;
+    }
+    setLoad(true);
+    const requestData = { email: mobileNumber };
+    resendOTP(requestData).then((res) => {
+      if (res.status === true) {
+        toast.success(res.message, {
+          position: "top-right",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+
+        setLoad(false);
+      } else {
+        toast.error(res.message, {
+          position: "top-right",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        setLoad(false);
+      }
+    });
+  };
+
+  const handleCartLogin = () => {
+    setCartOpen(false);
+  };
+
+  const handleHome = () => {
+    setCartOpen(false);
+    setOpen(true);
+  };
 
   return (
     <div>
-      <Header />
+    <TopHeader handleclear={() => handleclear(4)} loginStatus={loginStatus} />
+    <Header
+      code={countrytitle}
+      currency={countrycurrency}
+      flag={flag}
+      cartPrice={cartPrice}
+      cartProductlength={cartProduct}
+      curr={countrycurrency}
+      cartopen={cartOpen}
+      carthandleClose={carthandleClose}
+      carthandleOpen={carthandleOpen}
+      loginStatus={loginStatus}
+      handleOpen={() => setOpen(true)}
+      handleClose={() => setOpen(false)}
+      open={open}
+      showbtn={btn}
+      handleLogin={() => handleLogin()}
+      handleOTP={() => handleOTP()}
+      mobileNumber={mobileNumber}
+      handleMobileNumber={(e) => handleMobileNumber(e)}
+      sethandleOtp={(e) => sethandleOtp(e)}
+      otp={otp}
+      totalAmount={cartPrice}
+      store={store}
+      modalcurrency={countrycurrency}
+      handleclear={(index) => handleclear(index)}
+      // removeProduct={(id) => removeCartProduct(id)} removeLocalCart
+      removeProduct={(id) =>
+        loginStatus == true ? removeCartProduct(id) : removeLocalCart(id)
+      }
+      handleResendOTP={() => handleResendOTP()}
+      handleCartLogin={() => handleCartLogin()}
+      handleHome={() => handleHome()}
+    />
       <div className="pravacy_container">
         <div className="pravacy_content">
           <h2>Terms & Condition</h2>
