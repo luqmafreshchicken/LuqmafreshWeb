2d812cdbab5a51255c8892f6d54d3c637a8a60c9
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-08-09 16:19:16 +0530

    Update ImagesCard.js

--- src/customcomponent/imagescard/ImagesCard.js
+++ src/customcomponent/imagescard/ImagesCard.js
@@ -5,24 +5,39 @@ const ImagesCard = () => {
     <div className="mainimagecard">
       <div className="imagecard">
         <div className="imagecard1">
-          <img src="USP1.webp" />
-          <p>Premium produce, sourced directly from the origin</p>
+          <img src="quality.png" />
+          <div className="imagecard1_text">
+            <p>
+              Exquisite harvest, directly procured from its place of origin.
+            </p>
+          </div>
         </div>
         <div className="imagecard1">
-          <img src="USP2.webp" />
-          <p>Scientifically designed central production Unit</p>
+          <img src="store.png" />
+          <div className="imagecard1_text">
+            <p>
+              Specially crafted core manufacturing facility based on
+              scientific principles.
+            </p>
+          </div>
         </div>
         <div className="imagecard1">
-          <img src="USP3.webp" />
-          <p>Compliance to stringent quality checks</p>
+          <img src="badge.png" />
+          <div className="imagecard1_text">
+            <p>Adherence to rigorous quality inspections.</p>
+          </div>
         </div>
         <div className="imagecard1">
-          <img src="USP4.webp" />
-          <p>Delivered fresh everyday</p>
+          <img src="meat.png" />
+          <div className="imagecard1_text">
+            <p>Freshly delivered each day.</p>
+          </div>
         </div>
         <div className="imagecard1">
-          <img src="USP5.webp" />
-          <p>Experience extraordinary cooking</p>
+          <img src="frying-pan.png"  style={{height:"120px", width:"180px"}} />
+          <div className="imagecard1_text">
+            <p>Embark on an exceptional culinary journey</p>
+          </div>
         </div>
       </div>
     </div>
