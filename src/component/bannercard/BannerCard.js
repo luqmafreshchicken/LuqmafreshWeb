9683632c834d8fa992ffe5332d76dc95612da025
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-09-07 05:07:15 +0530

    Update BannerCard.js

--- src/component/bannercard/BannerCard.js
+++ src/component/bannercard/BannerCard.js
@@ -1,25 +1,13 @@
 import * as React from "react";
+import "react-slideshow-image/dist/styles.css";
+import { Slide } from "react-slideshow-image";
 import "./bannercard.css";
 import { useEffect, useState, useRef } from "react";
 import { BannerCard } from "../../serverRequest/Index";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import {
-  Navigation,
-  Pagination,
-  Scrollbar,
-  A11y,
-  Autoplay,
-  EffectFade,
-} from "swiper";
-import "swiper/css/navigation";
 import { NavLink } from "react-router-dom";
 
 export default function BannerCard1({ productId, to }) {
   const [banner, setBanner] = useState([]);
-
-  const swiperNavPrevRef = useRef(null);
-  const swiperNavNextRef = useRef(null);
 
   useEffect(() => {
     getbenner();
@@ -39,71 +27,37 @@ export default function BannerCard1({ productId, to }) {
   };
 
   return (
-    <div classname="main_banner_card" style={{ marginTop: "20px" }}>
-      <Swiper
-        modules={[
-          Navigation,
-          Autoplay,
-          Pagination,
-          Scrollbar,
-          A11y,
-          EffectFade,
-        ]}
-        spaceBetween={10}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          el: ".swiper-pagination",
-          clickable: true,
-        }}
-        // navigation={{
-        //   prevEl: "swiperNavPrevRef.current",
-        //   nextEl: "swiperNavNextRef.current",
-        // }}
-        effect="fade"
-        onInit={(swiper) => {
-          swiper.params.navigation.prevEl = swiperNavPrevRef.current;
-          swiper.params.navigation.nextEl = swiperNavNextRef.current;
-          swiper.navigation.init();
-          swiper.navigation.update();
-        }}
-        className="mySwiper"
-      >
-        {banner.map((item, index) => (
-          <SwiperSlide key={item._id}>
-            <div
-              style={{
-                backgroundImage: `url(${item?.image}) `,
-                display: "block",
-                width: "100%",
-                height: "100%",
-                objectfit: "cover",
-              }}
-              className="banner_sliderimg"
-            >
-              <div className="cashback_container">
+    <Slide autoplay={false}>
+      {banner.map((item) => (
+        <div className="each-slide-effect">
+          <div
+            style={{
+              backgroundImage: `url(${item?.image}) `,
+            }}
+            className="banner_slider"
+          >
+            <div className="cashback_container">
+              <div className="cashback_content">
                 <h1>{item?.title1}</h1>
-                <h2>{item?.title2}% OFF</h2>
+                <h2>{item?.title2} OFF</h2>
                 <h5>{item?.title3}</h5>
                 <div className="ban_discription">
                   <h6>{item?.description}</h6>
                 </div>
-                <NavLink to="/carddetail" state={{ id: item?.productId }}>
-                  <div className="order_shop_btn">
-                    <button onClick={() => handleShopNowClick(item?.productId)}>
-                      SHOP NOW
-                    </button>
-                  </div>
+                <NavLink to="/carddetail" state={{ 
+                  id:{
+                    id: item?.productId
+                  }
+                 }}>
+                  <button onClick={() => handleShopNowClick(item?.productId)}>
+                    SHOP NOW
+                  </button>
                 </NavLink>
               </div>
             </div>
-          </SwiperSlide>
-        ))}
-
-        <div className="swiper-pagination"> </div>
-      </Swiper>
-    </div>
+          </div>
+        </div>
+      ))}
+    </Slide>
   );
 }
