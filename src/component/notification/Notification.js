4fc4ce6d48399b58a31c271dff96377f6de44ffd
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-09-14 21:22:22 +0530

    Update Notification.js

--- src/component/notification/Notification.js
+++ src/component/notification/Notification.js
@@ -1,26 +1,39 @@
 import React from "react";
 import "./notification.css";
+import { useEffect } from "react";
+import { useState } from "react";
+import { getUserID, showNotification } from "../../serverRequest/Index";
 const Notification = () => {
+  const [notification, setNotification] = useState([]);
 
-    
+  useEffect(() => {
+    notiList();
+  }, []);
+
+  const notiList = async () => {
+    const UserId = await getUserID();
+    const res = await showNotification(UserId);
+    if (res.status == true) {
+      setNotification(res?.data);
+    } else {
+    }
+  };
+
   return (
     <div>
       <div className="notification_heading">
         <p>Notification</p>
       </div>
       <div className="show_notification_list">
-        <div className="show_notification_image">
-          <img src="ChickenLC.webp" />
-        </div>
-        <div className="show_notification_text">
-          <p>
-            Order cancelled<br/>
-            <span>Your order has been cancelled successfully</span>
-          </p>
-        </div>
-        <div className="show_notification_time">
-        <p>28/06/2023</p>
-        </div>
+      {notification.map((list, index) => (
+        <div className="show_notification_heading">
+        <h5>{list?.type}</h5>
+        <p>
+        {list?.message}
+        </p>
+        <span>20/09/2023</span>
+      </div>
+      ))}
       </div>
     </div>
   );
