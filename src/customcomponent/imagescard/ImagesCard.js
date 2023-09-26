9e14d11f197f3a672113b2c7aeba80c9e231fe37
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-09-27 01:58:58 +0530

    Update ImagesCard.js

--- src/customcomponent/imagescard/ImagesCard.js
+++ src/customcomponent/imagescard/ImagesCard.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./imagecard.css";
+import Image1 from "../../assest/Image/quality.png"
+import Image2 from "../../assest/Image/store.png"
+import Image3 from "../../assest/Image/badge.png"
+import Image4 from "../../assest/Image/meat.png"
+import Image5 from "../../assest/Image/frying-pan.png"
+
 const ImagesCard = () => {
   return (
     <div className="mainimagecard">
       <div className="imagecard">
         <div className="imagecard1">
-          <img src="quality.png" />
+          <img src={Image1} />
           <div className="imagecard1_text">
             <p>
               Exquisite harvest, directly procured from its place of origin.
@@ -13,7 +19,7 @@ const ImagesCard = () => {
           </div>
         </div>
         <div className="imagecard1">
-          <img src="store.png" />
+          <img src={Image2} />
           <div className="imagecard1_text">
             <p>
               Specially crafted core manufacturing facility based on
@@ -22,19 +28,19 @@ const ImagesCard = () => {
           </div>
         </div>
         <div className="imagecard1">
-          <img src="badge.png" />
+          <img src={Image3} />
           <div className="imagecard1_text">
             <p>Adherence to rigorous quality inspections.</p>
           </div>
         </div>
         <div className="imagecard1">
-          <img src="meat.png" />
+          <img src={Image4} />
           <div className="imagecard1_text">
             <p>Freshly delivered each day.</p>
           </div>
         </div>
         <div className="imagecard1">
-          <img src="frying-pan.png"  />
+          <img src={Image5}  />
           <div className="imagecard1_text">
             <p>Embark on an exceptional culinary journey</p>
           </div>
