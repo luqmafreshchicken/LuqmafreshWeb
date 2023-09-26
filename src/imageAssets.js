e1dbf27583bc764feb2fb6c6674b1f1d7b5b38fa
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-09-27 02:23:51 +0530

    Update imageAssets.js

--- src/imageAssets.js
+++ src/imageAssets.js
@@ -1,4 +1,8 @@
 export default {
-    app_logo: require('./assest/Image/MAIN LOGO.png'),
-    priceTag: require('./assest/Image/price-tag.png')
-  };
+  app_logo: require("./assest/Image/MAIN LOGO.png"),
+  priceTag: require("./assest/Image/price-tag.png"),
+  icon: require("./assest/Image/ICON LOGO.png"),
+  category: require("./assest/Image/application.png"),
+  cart: require("./assest/Image/cart1.png"),
+  account: require("./assest/Image/home.png"),
+};
