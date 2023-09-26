1675e2748eb0bb6fd65e46a923bccce69acea7b5
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-09-27 02:25:01 +0530

    Update MobileBottomtab.js

--- src/mobilecomponent/mobilebottomtab/MobileBottomtab.js
+++ src/mobilecomponent/mobilebottomtab/MobileBottomtab.js
@@ -2,47 +2,46 @@ import React, { useState } from "react";
 import "./mobilebottom.css";
 
 import { NavLink } from "react-router-dom";
-import ModalCart from "../../pages/modalcart/ModalCart";
+import imageAssets from "../../imageAssets";
 
-const MobileBottomtab = ({handleMobile}) => {
-  const [cartopen, setCartopen] = useState(false);
-
-  // const carthandleOpen = () => setCartopen(true);
-  // const carthandleClose = () => setCartopen(false);
+const MobileBottomtab = ({ handleMobile }) => {
   return (
     <div className="mobile_bottom_container">
       <div className="mobile_bottom">
-        <div className="bottom_tab1 " style={{marginBottom:"0.4rem"}}>
-          <NavLink to="/" className="mob_nav active" style={{display:"grid", placeItems:"center"}}>
-              <img src="ICON LOGO.png" height='39px' width='32px' />
+        <div className="bottom_tab1 " style={{ marginBottom: "0.4rem" }}>
+          <NavLink
+            to="/"
+            className="mob_nav active"
+            style={{ display: "grid", placeItems: "center" }}
+          >
+            <img src={imageAssets.icon} height="39px" width="32px" />
           </NavLink>
         </div>
 
         <div className="bottom_tab1">
           <NavLink to="/mobilecategorie" className="mob_nav">
             <div className="bottom_top_image">
-              <img src="application.png" className="home" />
+              <img src={imageAssets.category} className="home" />
             </div>
             <p>Categories</p>
           </NavLink>
         </div>
-        <div className="bottom_tab1" onClick={()=> handleMobile()}>
+        <div className="bottom_tab1" onClick={() => handleMobile()}>
           <div className="bottom_top_image">
-            <img src="cart1.png" className="home" />
+            <img src={imageAssets.cart} className="home" />
           </div>
           <p>My Cart</p>
         </div>
         <div className="bottom_tab1">
           <NavLink to="/mobileaccount" className="mob_nav">
             <div className="bottom_top_image">
-              <img src="home.png" className="home" />
+              <img src={imageAssets.account} className="home" />
             </div>
             <p>Account</p>
           </NavLink>
         </div>
       </div>
-    {/* <ModalCart cartopen={cartopen} carthandleClose={carthandleClose} />*/}
-      
+      {/* <ModalCart cartopen={cartopen} carthandleClose={carthandleClose} />*/}
     </div>
   );
 };
