1baa2743815cb495528fe6ff9f54489680957410
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-11-25 11:47:11 -0800

    Update SubCategorieList.js

--- src/pages/todaydeals/component/subcategorielist/SubCategorieList.js
+++ src/pages/todaydeals/component/subcategorielist/SubCategorieList.js
@@ -11,9 +11,7 @@ const SubCategorieList = ({ img, name, id, onclick,selID,catId }) => {
       {/* <div className="subcategorie_product_img_comtainer">
         <img src={img} />
   </div>*/}
-      <p style={{color:selID == catId ? 'white' : 'white', backgroundColor:selID == catId ? '#000' : '#ffbd31', padding:selID == catId ? '0.5rem 1rem' : '0.5rem 1rem', borderRadius:selID == catId ? '5px' : '5px' }}>{name}</p>
-      {/* <p className={selID == catId ? 'activeclass' : 'black'}>{name}</p> */}
-
+      <p style={{color:selID == catId ? 'red' : 'black' }}>{name}</p>
     </div>
   );
 };
