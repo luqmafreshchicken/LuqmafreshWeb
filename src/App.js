c7f617d41230801633200ff90a7b4bd73d041fd0
Kishan Tripathi <132270718+luqmafreshchicken@users.noreply.github.com>
2023-11-30 11:23:04 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,33 +10,41 @@ import GoToTop from "./customcomponent/gototop/GoToTop";
 import MobileBottomtab from "./mobilecomponent/mobilebottomtab/MobileBottomtab";
 import Routes from "./pages/routes/Routes";
 import "./App.css";
+import { useMediaQuery } from "react-responsive";
+import SplashPage from "./pages/splashpage/SplashPage";
 
 function App() {
-
+  const isMobile = useMediaQuery({ maxWidth: 900 });
 
   return (
     <div>
-      <Routes />
-      <div className="mobile_news_footer">
-        <NewsFooter />
-      </div>
-      <div className="mobile_support_section">
-        <SupportSection />
-      </div>
-      <div className="mobile_header_footer">
-        <HeaderFooter />
-      </div>
-      {/*<CenterFooter />*/}
-      <div className="mobile_contact_footer">
-        <ContactFooter />
-      </div>
-      {/* <City />*/}
-      <div className="mobile_gototop">
-        <GoToTop />
-      </div>
-      <div className="mobile_lower_footer">
-        <LowerFooter />
-      </div>
+      {isMobile ? (
+        <SplashPage />
+      ) : (
+        <>
+          <Routes />
+          <div className="mobile_news_footer">
+            <NewsFooter />
+          </div>
+          <div className="mobile_support_section">
+            <SupportSection />
+          </div>
+          <div className="mobile_header_footer">
+            <HeaderFooter />
+          </div>
+          {/*<CenterFooter />*/}
+          <div className="mobile_contact_footer">
+            <ContactFooter />
+          </div>
+          {/* <City />*/}
+          <div className="mobile_gototop">
+            <GoToTop />
+          </div>
+          <div className="mobile_lower_footer">
+            <LowerFooter />
+          </div>
+        </>
+      )}
     </div>
   );
 }
